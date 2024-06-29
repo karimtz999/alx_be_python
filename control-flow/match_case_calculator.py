32b9665c3389291daef0f9398fbeda19c7ef0c01
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-06-29 19:16:50 +0100

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,17 +1,21 @@
+
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
-if operation == '+':
-    result = num1 + num2
-elif operation == '-':
-    result = num1 - num2
-elif operation == '*':
-    result = num1 * num2
-elif operation == '/':
-    if num2 != 0:
-        result = num1 / num2
-    else:
-        result = "Cannot divide by zero."
 
+match operation:
+    case '+':
+        result = num1 + num2
+    case '-':
+        result = num1 - num2
+    case '*':
+        result = num1 * num2
+    case '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Cannot divide by zero."
+    case _:
+        result = "Invalid operation."
 
 print(f"The result is {result}.")
