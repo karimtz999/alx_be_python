07ae2fab713851e9cf008daa64907c09e2b9f5a9
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-06-29 19:35:43 +0100

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,10 +1,9 @@
-name = input("Enter your task: ")
-piriority = input("Priority (high/medium/low): ")
-time = input("Is it time-bound? (yes/no): ")
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
-if time == 'yes':
-    print(f"Reminder: '{name}' is a {piriority} priority task that requires immediate attention today!")
-else:
-    print(f"Note: '{name}' is a {piriority} priority task. Consider completing it when you have free time.")
-
-
+match time_bound: 
+ case'yes':
+    print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+ case 'no':
+    print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
