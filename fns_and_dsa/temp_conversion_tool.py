b019c583255562b03cfc193264216b708c936289
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-07-06 16:07:24 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,3 @@
-# temp_conversion_tool.py
-
 # Global conversion factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
@@ -34,3 +32,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
