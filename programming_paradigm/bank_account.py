461d0abf4c262b23eed21575578e939321965633
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-07-14 01:19:02 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,8 +3,8 @@ class InsufficientFundsError(Exception):
         return "Insufficient funds."
 
 class BankAccount:
-    def __init__(self):
-        self.account_balance = 250 
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
 
     def deposit(self, amount):
         self.account_balance += amount
@@ -14,7 +14,7 @@ class BankAccount:
             self.account_balance -= amount
             return True
         else:
-            return False
+            raise InsufficientFundsError()
 
     def display_balance(self):
         return f"Current Balance: ${self.account_balance}"
