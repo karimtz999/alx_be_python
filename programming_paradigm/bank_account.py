fe18a174c13953d4eadf51e376c359b051df3cde
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-07-14 23:19:58 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,19 @@
-# bank_account.py
-
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+        self.__account_balance = initial_balance
 
     def deposit(self, amount):
-        self.account_balance += amount
+        if amount > 0:
+            self.__account_balance += amount
 
     def withdraw(self, amount):
-        if amount <= self.account_balance:
-            self.account_balance -= amount
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
             return True
-        else:
-            return False
+        return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.__account_balance:.2f}")
 
+    def get_balance(self):
+        return self.__account_balance
