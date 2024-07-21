9e99632d902d6d78cf20c6e8a8e2f9406aa47923
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-07-21 18:22:28 +0100

    Add files via upload

---
+++ library_system.py
@@ -0,0 +1,34 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author} (eBook, {self.file_size}KB)"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author} (PrintBook, {self.page_count} pages)"
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
