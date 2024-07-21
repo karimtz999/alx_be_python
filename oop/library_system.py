0da6cdae2b018923aa39c84e665b6650f43cc3ef
ELFA <140125432+karimtz999@users.noreply.github.com>
2024-07-21 18:40:47 +0100

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,10 +1,12 @@
+
+
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
@@ -12,7 +14,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} (eBook, {self.file_size}KB)"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -20,7 +22,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} (PrintBook, {self.page_count} pages)"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
